b160090cbb4dbc5ab2e65964b7a50e7918b27d03
Norvez <normanalvarado123@gmail.com>
2024-05-01 18:17:41 -0600

    a ver

--- src/components/ProductsDetails.tsx
+++ src/components/ProductsDetails.tsx
@@ -34,43 +34,7 @@ const ProductDetails: React.FC = () => {
             <tbody>
             <div>{product.id}</div>
             <div>{product.title}</div>
-          { /*   <tr>
-                <th>ID</th>
-                <td>{item.id}</td>
-              </tr>
-              <tr>
-                <th>Título</th>
-                <td>{item.title}</td>
-              </tr>
-              <tr>
-                <th>Precio</th>
-                <td>${item.price}</td>
-              </tr>
-              <tr>
-                <th>Descripción</th>
-                <td>{item.description}</td>
-              </tr>
-              <tr>
-                <th>Categoría</th>
-                <td>{item.category}</td>
-              </tr>
-              <tr>
-                <th>Imágenes</th>
-                <td>
-                  <div className="d-flex flex-wrap">
-                    {item.images.map((image, index) => (
-                      <img
-                        key={index}
-                        src={image}
-                        alt={`Imagen ${index + 1}`}
-                        className="img-thumbnail m-1"
-                        style={{ width: '50px', height: '50px' }}
-                      />
-                    ))}
-                  </div>
-                </td>
-              </tr>
-              */}
+         
             </tbody>
           </table>
         </div>
