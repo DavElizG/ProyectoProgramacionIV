55e853304ac7069aef8f7e23072d2f13432441b2
nixonjaen <nixon.jaen.alfaro@est.una.ac.cr>
2024-05-06 13:41:43 -0600

    cambios

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,17 +1,28 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode, createContext, useState } from 'react';
 import MyContext from './MyContext';
 
-//COMPLETAR EL PROVIDER CON LO QUE NECESITE
+interface MyContextType {
+  productId: number;
+  setProductId: React.Dispatch<React.SetStateAction<number>>;
+  pageNumber: number;
+  setPageNumber: React.Dispatch<React.SetStateAction<number>>;
+}
 
-const ProductsProvider = ({children}: {children: ReactNode}) => {
-  const [productId, setProductId] = useState(0)
-  const [pageNumber, setPageNumber] = useState(0)
+const ProductsProvider = ({ children }: { children: ReactNode }) => {
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const contextValue: MyContextType = {
+    productId: 0, // Aquí puedes inicializar productId como lo necesites
+    setProductId: () => {}, // Aquí puedes inicializar setProductId como lo necesites
+    pageNumber,
+    setPageNumber
+  };
 
   return (
-    <MyContext.Provider value={{productId, setProductId, pageNumber, setPageNumber}}>
-        {children}
+    <MyContext.Provider value={contextValue}>
+      {children}
     </MyContext.Provider>
-  )
-}
+  );
+};
 
 export default ProductsProvider;
