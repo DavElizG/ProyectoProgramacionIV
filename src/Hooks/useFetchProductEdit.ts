7995942e8b355756ea399db3ad6df77c7032a2ea
Jose Guadamuz <jose.guadamuz.elizondo@est.una.ac.cr>
2024-05-08 19:32:14 -0600

    chore: Refactor useFetchProductEdit to remove unused code and handle image changes in ProductEdit component

--- src/Hooks/useFetchProductEdit.ts
+++ src/Hooks/useFetchProductEdit.ts
@@ -1,20 +1,12 @@
 // useFetchProductEdit.ts
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getProductsById, updateProduct } from '../services/ProductService';
+import { getProductsById } from '../services/ProductService';
 import { Product } from '../types/Product';
 
 const useFetchProductEdit = () => {
   const { productId } = useParams<{ productId: string }>();
   const [product, setProduct] = useState<Product>({
-    id: 0,
-    title: '',
-    price: 0,
-    description: '',
-    category: 0,
-    images: []
-  });
-  const [formData, setFormData] = useState<Product>({
     id: 0,
     title: '',
     price: 0,
@@ -30,7 +22,6 @@ const useFetchProductEdit = () => {
       try {
         const { data } = await getProductsById(productId);
         setProduct(data);
-        setFormData(data);
         setLoading(false);
       } catch (error) {
         setError(error.message);
@@ -41,32 +32,7 @@ const useFetchProductEdit = () => {
     fetchProduct();
   }, [productId]);
 
-  const handleUpdateProduct = async () => {
-    try {
-      await updateProduct(productId || '', formData);
-      setProduct(formData);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
-
-  const handleImagesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const images = e.target.value.split(',').map(image => image.trim());
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      images,
-    }));
-  };
-
-  return { product, formData, setFormData, loading, error, handleUpdateProduct, handleInputChange, handleImagesChange };
+  return { product, loading, error };
 };
 
 export default useFetchProductEdit;
